32bc326a2c08fa5a3eaa7c0a8c37eab62e7bd8f9
TsilaAllaoui <tsila.allaoui@gmail.com>
2023-10-16 19:11:55 +0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PharmacyMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
